b1c5846854ec51afebe13b8d368dc2e737a8b60f
Wiggidy <rlr0017@auburn.edu>
2023-10-18 19:45:19 -0500

    change null anti cache key + divide shared keys

--- etco-go/client/cachekeys/cachekeys.go
+++ etco-go/client/cachekeys/cachekeys.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 
 	b "github.com/WiggidyW/etco-go-bucket"
+	builder "github.com/WiggidyW/etco-go/buildconstants"
 )
 
 const (
-	WRITE_BUYBACK_APPRAISAL_NULL_ANTI_CACHE_KEY string = "WBA_NULL"
+	NULL_ANTI_CACHE_KEY string = "_ACNULL"
 
 	CONTRACTS_CACHE_KEY              string = "contracts"
 	JWKS_CACHE_KEY                   string = "jwks"
@@ -26,6 +27,7 @@ const (
 	WEB_BUYBACK_BUNDLE_KEYS_PREFIX string = "bundlekeys"
 	WEB_SHOP_BUNDLE_KEYS_PREFIX    string = "bundlekeys"
 	AUTH_HASH_SET_READER_PREFIX    string = "authhashset"
+	BUCKET_READER_PREFIX           string = "bucket"
 
 	IS_BUY_TRUE  string = "b"
 	IS_BUY_FALSE string = "s"
@@ -33,20 +35,18 @@ const (
 
 func WriteBuybackAppraisalAntiCacheKey(characterId *int32) string {
 	if characterId == nil {
-		return WRITE_BUYBACK_APPRAISAL_NULL_ANTI_CACHE_KEY
+		return NULL_ANTI_CACHE_KEY
 	} else {
 		return ReadUserDataCacheKey(*characterId)
 	}
 }
 
-func AuthHashSetReaderCacheKey(authDomain string) string {
-	return fmt.Sprintf("%s-%s", AUTH_HASH_SET_READER_PREFIX, authDomain)
+// SHARED - JWKS is static
+func JWKSCacheKey() string {
+	return JWKS_CACHE_KEY
 }
 
-func BucketReaderCacheKey(objName string) string {
-	return objName
-}
-
+// SHARED - contractIds are unique
 func ContractItemsCacheKey(contractId int32) string {
 	return fmt.Sprintf(
 		"%s-%d",
@@ -55,54 +55,7 @@ func ContractItemsCacheKey(contractId int32) string {
 	)
 }
 
-func ContractsCacheKey() string {
-	return CONTRACTS_CACHE_KEY
-}
-
-func JWKSCacheKey() string {
-	return JWKS_CACHE_KEY
-}
-
-func CharacterInfoCacheKey(characterId int32) string {
-	return fmt.Sprintf(
-		"%s-%d",
-		CHARACTER_INFO_PREFIX,
-		characterId,
-	)
-}
-
-func CorporationInfoCacheKey(corporationId int32) string {
-	return fmt.Sprintf(
-		"%s-%d",
-		CORPORATION_INFO_PREFIX,
-		corporationId,
-	)
-}
-
-func AllianceInfoCacheKey(allianceId int32) string {
-	return fmt.Sprintf(
-		"%s-%d",
-		ALLIANCE_INFO_PREFIX,
-		allianceId,
-	)
-}
-
-func StructureInfoCacheKey(structureId int64) string {
-	return fmt.Sprintf(
-		"%s-%d",
-		STRUCTURE_INFO_PREFIX,
-		structureId,
-	)
-}
-
-func AllShopAssetsCacheKey() string {
-	return ALL_SHOP_ASSETS_CACHE_KEY
-}
-
-func UnreservedShopAssetsCacheKey() string {
-	return UNRESERVED_SHOP_ASSETS_CACHE_KEY
-}
-
+// SHARED - market results are the same for all corps
 func RegionMarketCacheKey(
 	regionId int32,
 	typeId int32,
@@ -116,7 +69,6 @@ func RegionMarketCacheKey(
 		isBuyStr(isBuy),
 	)
 }
-
 func FilterRegionMarketCacheKey(
 	regionId int32,
 	typeId int32,
@@ -129,7 +81,6 @@ func FilterRegionMarketCacheKey(
 		locationId,
 	)
 }
-
 func StructureMarketCacheKey(structureId int64) string {
 	return fmt.Sprintf(
 		"%s-%d",
@@ -137,7 +88,6 @@ func StructureMarketCacheKey(structureId int64) string {
 		structureId,
 	)
 }
-
 func FilterStructureMarketCacheKey(
 	structureId int64,
 	typeId int32,
@@ -151,57 +101,152 @@ func FilterStructureMarketCacheKey(
 	)
 }
 
-func ReadAppraisalCacheKey(appraisalCode string) string {
-	return appraisalCode
-	// return fmt.Sprintf("appraisal-%s", appraisalCode)
-}
-
-func ReadUserDataCacheKey(characterId int32) string {
+// SHARED - entity info results are the same for all corps
+func CharacterInfoCacheKey(characterId int32) string {
 	return fmt.Sprintf(
 		"%s-%d",
+		CHARACTER_INFO_PREFIX,
+		characterId,
+	)
+}
+func CorporationInfoCacheKey(corporationId int32) string {
+	return fmt.Sprintf(
+		"%s-%d",
+		CORPORATION_INFO_PREFIX,
+		corporationId,
+	)
+}
+func AllianceInfoCacheKey(allianceId int32) string {
+	return fmt.Sprintf(
+		"%s-%d",
+		ALLIANCE_INFO_PREFIX,
+		allianceId,
+	)
+}
+
+// NOT SHARED - each corp has their own contracts
+func ContractsCacheKey() string {
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		CONTRACTS_CACHE_KEY,
+	)
+}
+
+// NOT SHARED - structure info depends upon docking rights, which are corp-specific
+func StructureInfoCacheKey(structureId int64) string {
+	return fmt.Sprintf(
+		"%d-%s-%d",
+		builder.CORPORATION_ID,
+		STRUCTURE_INFO_PREFIX,
+		structureId,
+	)
+}
+
+// NOT SHARED - each corp has their own assets
+func AllShopAssetsCacheKey() string {
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		ALL_SHOP_ASSETS_CACHE_KEY,
+	)
+}
+func UnreservedShopAssetsCacheKey() string {
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		UNRESERVED_SHOP_ASSETS_CACHE_KEY,
+	)
+}
+
+// NOT SHARED - corp-specific +
+// negligible Sprintf performance cost reduces negligible hash collision odds
+func ReadAppraisalCacheKey(appraisalCode string) string {
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		appraisalCode,
+	)
+}
+
+// NOT SHARED - userData is corp-specific
+func ReadUserDataCacheKey(characterId int32) string {
+	return fmt.Sprintf(
+		"%d-%s-%d",
+		builder.CORPORATION_ID,
 		READ_USER_DATA_PREFIX,
 		characterId,
 	)
 }
 
+// NOT SHARED - each corp has their own shop queue
 func ReadShopQueueCacheKey() string {
-	return SHOP_QUEUE_READ_CACHE_KEY
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		SHOP_QUEUE_READ_CACHE_KEY,
+	)
 }
 
+// NOT SHARED - bucket data is corp-specific
+func AuthHashSetReaderCacheKey(authDomain string) string {
+	return fmt.Sprintf(
+		"%d-%s-%s",
+		builder.CORPORATION_ID,
+		AUTH_HASH_SET_READER_PREFIX,
+		authDomain,
+	)
+}
 func WebBuybackSystemTypeMapsBuilderReaderCacheKey() string {
-	return b.OBJNAME_WEB_BUYBACK_SYSTEM_TYPE_MAPS_BUILDER
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		b.OBJNAME_WEB_BUYBACK_SYSTEM_TYPE_MAPS_BUILDER,
+	)
 }
-
 func WebBuybackBundleKeysCacheKey() string {
 	return fmt.Sprintf(
-		"%s-%s",
+		"%d-%s-%s",
+		builder.CORPORATION_ID,
 		WEB_BUYBACK_BUNDLE_KEYS_PREFIX,
 		b.OBJNAME_WEB_BUYBACK_SYSTEM_TYPE_MAPS_BUILDER,
 	)
 }
-
 func WebShopBundleKeysCacheKey() string {
 	return fmt.Sprintf(
-		"%s-%s",
+		"%d-%s-%s",
+		builder.CORPORATION_ID,
 		WEB_SHOP_BUNDLE_KEYS_PREFIX,
 		b.OBJNAME_WEB_SHOP_LOCATION_TYPE_MAPS_BUILDER,
 	)
 }
-
 func WebShopLocationTypeMapsBuilderReaderCacheKey() string {
-	return b.OBJNAME_WEB_SHOP_LOCATION_TYPE_MAPS_BUILDER
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		b.OBJNAME_WEB_SHOP_LOCATION_TYPE_MAPS_BUILDER,
+	)
 }
-
 func WebBuybackSystemsReaderCacheKey() string {
-	return b.OBJNAME_WEB_BUYBACK_SYSTEMS
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		b.OBJNAME_WEB_BUYBACK_SYSTEMS,
+	)
 }
-
 func WebShopLocationsReaderCacheKey() string {
-	return b.OBJNAME_WEB_SHOP_LOCATIONS
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		b.OBJNAME_WEB_SHOP_LOCATIONS,
+	)
 }
-
 func WebMarketsReaderCacheKey() string {
-	return b.OBJNAME_WEB_MARKETS
+	return fmt.Sprintf(
+		"%d-%s",
+		builder.CORPORATION_ID,
+		b.OBJNAME_WEB_MARKETS,
+	)
 }
 
 // // util
