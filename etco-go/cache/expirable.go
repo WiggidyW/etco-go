53f30f9de53296808c893e13722159b55e3db7b3
Wiggidy <rlr0017@auburn.edu>
2023-10-22 12:35:39 -0500

    add non-caching option

--- etco-go/cache/expirable.go
+++ etco-go/cache/expirable.go
@@ -5,6 +5,7 @@ import "time"
 type Expirable[D any] interface {
 	Data() D
 	Expires() time.Time
+	Cache() bool
 }
 
 type ExpirableData[D any] struct {
@@ -26,4 +27,34 @@ func (ed ExpirableData[D]) Expires() time.Time {
 
 func (ed ExpirableData[D]) Data() D {
 	return ed.Ddata
+}
+
+func (ExpirableData[D]) Cache() bool {
+	return true
+}
+
+type MaybeCacheExpirableData[D any] struct {
+	Ddata    D
+	Eexpires time.Time
+	Ccache   bool
+}
+
+func NewMaybeCacheExpirableData[D any](data D, expiry time.Time, cache bool) MaybeCacheExpirableData[D] {
+	return MaybeCacheExpirableData[D]{data, expiry, cache}
+}
+
+func NewMaybeCacheExpirableDataPtr[D any](data D, expiry time.Time, cache bool) *MaybeCacheExpirableData[D] {
+	return &MaybeCacheExpirableData[D]{data, expiry, cache}
+}
+
+func (ed MaybeCacheExpirableData[D]) Expires() time.Time {
+	return ed.Eexpires
+}
+
+func (ed MaybeCacheExpirableData[D]) Data() D {
+	return ed.Ddata
+}
+
+func (ed MaybeCacheExpirableData[D]) Cache() bool {
+	return ed.Ccache
 }
