3083e4423ce6c5c664bf2d7f3f2ea71d24a5bf9b
Wiggidy <rlr0017@auburn.edu>
2023-11-20 19:58:58 -0600

    remove unnecessary constructors and add DEV_MODE

--- etco-go/main.go
+++ etco-go/main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"fmt"
 	"net"
-	"net/http"
 	"os"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
-	"github.com/WiggidyW/etco-go/bucket"
 	build "github.com/WiggidyW/etco-go/buildconstants"
-	"github.com/WiggidyW/etco-go/cache"
 	"github.com/WiggidyW/etco-go/logger"
 	"github.com/WiggidyW/etco-go/proto"
-	rdb "github.com/WiggidyW/etco-go/remotedb"
 	"github.com/WiggidyW/etco-go/service"
 )
 
@@ -34,44 +30,23 @@ func getAddr() string {
 func main() {
 	timeStart := time.Now()
 
-	// initialize basal clients, upon which service inner clients are built
-	cCache := cache.NewSharedClientCache(
-		build.CCACHE_MAX_BYTES,
-	)
-	sCache := cache.NewSharedServerCache(
-		build.SCACHE_ADDRESS,
-	)
-	rBucketClient := bucket.NewBucketClient(
-		build.BUCKET_NAMESPACE,
-		[]byte(build.BUCKET_CREDS_JSON),
-	)
-	rRDBClient := rdb.NewRemoteDBClient(
-		[]byte(build.REMOTEDB_CREDS_JSON),
-		build.REMOTEDB_PROJECT_ID,
-	)
-	httpClient := &http.Client{} // TODO: timeouts are defined per-method
-
 	// initialize the service, which implements all protobuf methods
-	service := service.NewService(
-		cCache,
-		sCache,
-		rBucketClient,
-		rRDBClient,
-		httpClient,
-	)
-
-	// DELETE THIS DO NOT COMMIT //
-	creds, err := credentials.NewServerTLSFromFile(
-		"cert.pem",
-		"key.pem",
-	)
-	if err != nil {
-		panic(err)
-	}
-	grpcServer := grpc.NewServer(grpc.Creds(creds))
+	service := service.NewService()
 
 	// create the GRPC server and register the service
-	// grpcServer := grpc.NewServer()
+	var grpcServer *grpc.Server
+	if build.DEV_MODE {
+		creds, err := credentials.NewServerTLSFromFile(
+			"cert.pem",
+			"key.pem",
+		)
+		if err != nil {
+			panic(err)
+		}
+		grpcServer = grpc.NewServer(grpc.Creds(creds))
+	} else {
+		grpcServer = grpc.NewServer()
+	}
 	proto.RegisterEveTradingCoServer(grpcServer, service)
 
 	listener, err := net.Listen("tcp", getAddr())
