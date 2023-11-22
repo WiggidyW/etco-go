5c39eb273352d7c9e9a6355ba55168222bd901fe
Wiggidy <rlr0017@auburn.edu>
2023-11-22 14:55:14 -0600

    Refactor Locks to behave more like context.Context

--- etco-go/cache/context.go
+++ etco-go/cache/context.go
@@ -9,31 +9,119 @@ import (
 	"github.com/WiggidyW/etco-go/cache/keys"
 )
 
-type ContextLocks struct {
-	locks  map[[16]byte]*Lock
+type ScopedLocks struct {
 	ctx    context.Context
 	cancel context.CancelFunc
+
+	parent *ScopedLocks
+	locks  map[[16]byte]*Lock
+	scope  int64
 	mu     *sync.RWMutex
 }
 
+func newScopedLocks() ScopedLocks {
+	ctx, cancel := context.WithCancel(context.Background())
+	return ScopedLocks{
+		parent: nil,
+		ctx:    ctx,
+		cancel: cancel,
+		locks:  make(map[[16]byte]*Lock),
+		scope:  0,
+		mu:     new(sync.RWMutex),
+	}
+}
+
+func (sl ScopedLocks) withNewScope() ScopedLocks {
+	sl.mu.RLock()
+	locks := make(map[[16]byte]*Lock, len(sl.locks))
+	for key, lock := range sl.locks {
+		locks[key] = lock
+	}
+	sl.mu.RUnlock()
+
+	sl.locks = locks
+	sl.scope = sl.newScope()
+	sl.mu = new(sync.RWMutex)
+	return sl
+}
+
+func (sl ScopedLocks) newScope() int64 {
+	return rand.Int63()
+}
+
+func (sl ScopedLocks) unlock(key, typeStr keys.Key) {
+	sl.mu.RLock()
+	lock, ok := sl.locks[key.Bytes16()]
+	sl.mu.RUnlock()
+	if ok {
+		go sl.localUnlock(lock)
+		go sl.serverUnlock(lock)
+	}
+}
+
+func (sl ScopedLocks) unlockScoped() {
+	sl.mu.RLock()
+	defer sl.mu.RUnlock()
+	for _, lock := range sl.locks {
+		go sl.localUnlock(lock)
+		go sl.serverUnlock(lock)
+	}
+}
+
+func (sl ScopedLocks) unlockAll() { sl.cancel() }
+
+func (sl ScopedLocks) getLockForChild(key, typeStr keys.Key) (
+	lock *Lock,
+	ok bool,
+) {
+	sl.mu.RLock()
+	lock, ok = sl.locks[key.Bytes16()]
+	sl.mu.RUnlock()
+	if !ok && sl.parent != nil {
+		return sl.parent.getLockForChild(key, typeStr)
+	} else {
+		return lock, ok
+	}
+}
+
+func (sl ScopedLocks) getLock(key, typeStr keys.Key) (lock *Lock) {
+	var ok bool
+	sl.mu.Lock()
+	defer sl.mu.Unlock()
+	lock, ok = sl.locks[key.Bytes16()]
+	if !ok && sl.parent != nil {
+		lock, ok = sl.parent.getLockForChild(key, typeStr)
+		if ok {
+			sl.locks[key.Bytes16()] = lock
+		}
+	}
+	if !ok {
+		lock = newLock(sl.ctx, key, typeStr)
+		sl.locks[key.Bytes16()] = lock
+	}
+	return lock
+}
+
+func (sl ScopedLocks) localLock(lock *Lock) error {
+	return lock.localLock(sl.scope)
+}
+func (sl ScopedLocks) serverLock(lock *Lock) error {
+	return lock.serverLock(sl.scope)
+}
+func (sl ScopedLocks) localUnlock(lock *Lock) {
+	lock.localUnlock(sl.scope)
+}
+func (sl ScopedLocks) serverUnlock(lock *Lock) {
+	lock.serverUnlock(sl.scope)
+}
+
 type Context struct {
-	locks *ContextLocks
-	ctx   context.Context
-	scope int64
+	ctx         context.Context
+	scopedLocks ScopedLocks
 }
 
 func NewContext(ctx context.Context) Context {
-	locksCtx, locksCancel := context.WithCancel(context.Background())
-	return Context{
-		locks: &ContextLocks{
-			locks:  make(map[[16]byte]*Lock),
-			mu:     new(sync.RWMutex),
-			ctx:    locksCtx,
-			cancel: locksCancel,
-		},
-		ctx:   ctx,
-		scope: 0,
-	}
+	return Context{ctx: ctx, scopedLocks: newScopedLocks()}
 }
 
 func (x Context) Ctx() context.Context { return x.ctx }
@@ -57,7 +145,7 @@ func (x Context) WithTimeout(timeout time.Duration) (
 }
 
 func (x Context) WithNewScope() Context {
-	x.scope = x.newScope()
+	x.scopedLocks = x.scopedLocks.withNewScope()
 	return x
 }
 
@@ -67,42 +155,26 @@ func (x Context) Background() Context {
 }
 
 func (x Context) Unlock(key, typeStr keys.Key) {
-	x.locks.mu.RLock()
-	lock, ok := x.locks.locks[key.Bytes16()]
-	x.locks.mu.RUnlock()
-	if ok {
-		go x.localUnlock(lock)
-		go x.serverUnlock(lock)
-	}
+	x.scopedLocks.unlock(key, typeStr)
 }
-
-func (x Context) UnlockAll() { x.locks.cancel() }
-
+func (x Context) UnlockAll() {
+	x.scopedLocks.unlockAll()
+}
 func (x Context) UnlockScoped() {
-	x.locks.mu.RLock()
-	defer x.locks.mu.RUnlock()
-	for _, lock := range x.locks.locks {
-		go lock.localUnlock(x.scope)
-		go lock.serverUnlock(x.scope)
-	}
+	x.scopedLocks.unlockScoped()
 }
-
 func (x Context) getLock(key, typeStr keys.Key) (lock *Lock) {
-	var lockOk bool
-	x.locks.mu.RLock()
-	lock, lockOk = x.locks.locks[key.Bytes16()]
-	x.locks.mu.RUnlock()
-	if !lockOk {
-		lock = newLock(x.locks.ctx, key, typeStr)
-		x.locks.mu.Lock()
-		defer x.locks.mu.Unlock()
-		x.locks.locks[key.Bytes16()] = lock
-	}
-	return lock
+	return x.scopedLocks.getLock(key, typeStr)
 }
-
-func (x Context) localLock(lock *Lock) error  { return lock.localLock(x.scope) }
-func (x Context) serverLock(lock *Lock) error { return lock.serverLock(x.scope) }
-func (x Context) localUnlock(lock *Lock)      { lock.localUnlock(x.scope) }
-func (x Context) serverUnlock(lock *Lock)     { lock.serverUnlock(x.scope) }
-func (x Context) newScope() int64             { return rand.Int63() }
+func (x Context) localLock(lock *Lock) error {
+	return x.scopedLocks.localLock(lock)
+}
+func (x Context) serverLock(lock *Lock) error {
+	return x.scopedLocks.serverLock(lock)
+}
+func (x Context) localUnlock(lock *Lock) {
+	x.scopedLocks.localUnlock(lock)
+}
+func (x Context) serverUnlock(lock *Lock) {
+	x.scopedLocks.serverUnlock(lock)
+}
