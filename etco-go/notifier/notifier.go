0c4fc5dd3c6602df01cbbf1923956cb2aa2f60f3
Wiggidy <rlr0017@auburn.edu>
2023-11-22 17:34:06 -0600

    Refactor notifier package and add Discord integration

--- etco-go/notifier/notifier.go
+++ etco-go/notifier/notifier.go
@@ -3,41 +3,115 @@ package notifier
 import (
 	"context"
 	"fmt"
+	"sync"
+	"time"
 
 	build "github.com/WiggidyW/etco-go/buildconstants"
+	"github.com/WiggidyW/etco-go/logger"
 
 	"github.com/nikoksr/notify"
+	"github.com/nikoksr/notify/service/discord"
+)
+
+const (
+	CHARACTER_LIMIT int = 2000
+
+	MAX_DISCORD_ATTEMPTS int           = 3
+	DISCORD_SLEEP_INTVL  time.Duration = 5 * time.Second
 )
 
 var (
-	BuybackContractNotifier notify.Notifier
-	ShopContractNotifier    notify.Notifier
-	PurchaseNotifier        notify.Notifier
+	mu *sync.RWMutex
 )
 
 func init() {
-	BuybackContractNotifier = notify.New()
-	ShopContractNotifier = notify.New()
-	PurchaseNotifier = notify.New()
+	mu = new(sync.RWMutex)
+	mu.Lock()
+	go func() {
+		var wg sync.WaitGroup
+		if build.DISCORD_BOT_TOKEN != "" && build.DISCORD_CHANNEL != "" {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				err := discordInit(0)
+				if err != nil {
+					logger.Err(err.Error())
+				}
+			}()
+		}
+		wg.Wait()
+		mu.Unlock()
+	}()
+}
+
+func discordInit(attempt int) (err error) {
+	discord := discord.New()
+	discord.AddReceivers(build.DISCORD_CHANNEL)
+	err = discord.AuthenticateWithBotToken(build.DISCORD_BOT_TOKEN)
+	if err == nil {
+		notify.UseServices(discord)
+		return nil
+	} else if attempt >= MAX_DISCORD_ATTEMPTS {
+		return fmt.Errorf("failed to authenticate with discord: %w", err)
+	}
+	time.Sleep(DISCORD_SLEEP_INTVL)
+	return discordInit(attempt + 1)
 }
 
 func urlListSend(
 	subject string,
-	notifier notify.Notifier,
 	ctx context.Context,
 	baseUrl string,
 	affixes ...string,
-) error {
-	message := ""
+) (
+	err error,
+) {
+	mu.RLock()
+	defer mu.RUnlock()
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf(
+				"failed to send '%s' notification: %w",
+				subject,
+				err,
+			)
+		}
+	}()
+
+	lenSubject := len(subject)
+	if lenSubject > CHARACTER_LIMIT {
+		err = fmt.Errorf("subject '%s' is too long", subject)
+		return err
+	}
+
+	messages := make([]string, 0, 1)
+	var message, affixMessage string
+	var affixSumLen int
+
 	for _, affix := range affixes {
-		message += baseUrl + affix + "\n"
+		affixMessage = baseUrl + affix + "\n"
+		affixSumLen = lenSubject + len(affixMessage)
+		if len(message)+affixSumLen > CHARACTER_LIMIT {
+			if affixSumLen > CHARACTER_LIMIT {
+				err = fmt.Errorf("affix '%s' is too long to send", affix)
+				return err
+			} else {
+				messages = append(messages, message)
+				message = affixMessage
+			}
+		} else {
+			message += affixMessage
+		}
 	}
-	err := notifier.Send(ctx, subject, message)
-	if err != nil {
-		return fmt.Errorf("failed to send '%s' notification: %w", subject, err)
-	} else {
-		return nil
+	messages = append(messages, message)
+
+	for _, message := range messages {
+		err := notify.Send(ctx, subject, message)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
 func BuybackContractsSend(
@@ -46,7 +120,6 @@ func BuybackContractsSend(
 ) error {
 	return urlListSend(
 		"New Buyback Contracts",
-		BuybackContractNotifier,
 		ctx,
 		build.BUYBACK_CONTRACT_NOTIFICATIONS_BASE_URL,
 		affixes...,
@@ -59,7 +132,6 @@ func ShopContractsSend(
 ) error {
 	return urlListSend(
 		"New Shop Contracts",
-		ShopContractNotifier,
 		ctx,
 		build.SHOP_CONTRACT_NOTIFICATIONS_BASE_URL,
 		affixes...,
@@ -72,7 +144,6 @@ func PurchasesSend(
 ) error {
 	return urlListSend(
 		"New Purchases",
-		PurchaseNotifier,
 		ctx,
 		build.PURCHASE_NOTIFICATIONS_BASE_URL,
 		affixes...,
