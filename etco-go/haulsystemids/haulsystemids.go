666fe9bc01ea12207ce4dce32cdbf7e758f103e4
Wiggidy <rlr0017@auburn.edu>
2023-12-03 18:24:15 -0600

    Remove unused ToInt64 function

--- etco-go/haulsystemids/haulsystemids.go
+++ etco-go/haulsystemids/haulsystemids.go
@@ -4,8 +4,3 @@ type HaulSystemIds struct {
 	Start int32
 	End   int32
 }
-
-// only used for hashing
-func (hsi HaulSystemIds) ToInt64() (i int64) {
-	return int64(hsi.Start)<<32 | int64(hsi.End)
-}
