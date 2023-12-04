53f66284cd0a05a84741c513fb11af2fffb8e162
Wiggidy <rlr0017@auburn.edu>
2023-12-03 18:25:56 -0600

    Add AllHaulRoutes function to retrieve haul routes

--- etco-go/service/data_systems.go
+++ etco-go/service/data_systems.go
@@ -6,7 +6,35 @@ import (
 	"github.com/WiggidyW/etco-go/proto"
 	"github.com/WiggidyW/etco-go/protoregistry"
 	"github.com/WiggidyW/etco-go/staticdb"
+
+	b "github.com/WiggidyW/etco-go-bucket"
 )
+
+func (Service) AllHaulRoutes(
+	_ context.Context,
+	_ *proto.EmptyRequest,
+) (
+	rep *proto.HaulRoutesResponse,
+	_ error,
+) {
+	systemInfos := staticdb.UNSAFE_GetSystemInfos()
+	haulRoutes := staticdb.UNSAFE_GetHaulRoutes()
+	routes := make([]*proto.HaulRouteInfo, len(haulRoutes))
+	r := protoregistry.NewProtoRegistry(len(haulRoutes))
+
+	i := 0
+	for key := range haulRoutes {
+		startSystemId, endSystemId := staticdb.GetHaulRouteSystems(key)
+		routes[i] = &proto.HaulRouteInfo{
+			StartSystemInfo: registryAddSystem(systemInfos, r, startSystemId),
+			EndSystemInfo:   registryAddSystem(systemInfos, r, endSystemId),
+		}
+		i++
+	}
+
+	rep = &proto.HaulRoutesResponse{Routes: routes, Strs: r.Finish()}
+	return rep, nil
+}
 
 func (Service) AllSystems(
 	_ context.Context,
@@ -53,14 +81,20 @@ func systems(systemIds []int32) *proto.SystemsResponse {
 	systemInfos := staticdb.UNSAFE_GetSystemInfos()
 	systems := make([]*proto.SystemInfo, len(systemIds))
 	r := protoregistry.NewProtoRegistry(len(systemIds))
-
 	for i, systemId := range systemIds {
-		if systemInfo, ok := systemInfos[systemId]; ok {
-			systems[i] = r.UNSAFE_AddSystem(systemId, systemInfo)
-		} else {
-			systems[i] = r.AddUndefinedSystem(systemId)
-		}
+		systems[i] = registryAddSystem(systemInfos, r, systemId)
 	}
-
 	return &proto.SystemsResponse{Systems: systems, Strs: r.Finish()}
+}
+
+func registryAddSystem(
+	systemInfos map[int32]b.System,
+	r *protoregistry.ProtoRegistry,
+	systemId int32,
+) *proto.SystemInfo {
+	if systemInfo, ok := systemInfos[systemId]; ok {
+		return r.UNSAFE_AddSystem(systemId, systemInfo)
+	} else {
+		return r.AddUndefinedSystem(systemId)
+	}
 }
